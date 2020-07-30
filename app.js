71acfc74eba60f07e3bb83a9aa6ce83f87c23101
Ben <baka_1993@yahoo.co.uk>
2020-07-30 17:39:46 +0100

    Build Web Server

--- app.js
+++ app.js
@@ -41,6 +41,8 @@ passport.use(User.createStrategy());
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
+//For Next Time: Start aligning header text on small-screen devices!
+
 app.get("/", (req, res) => {
   res.render("home");
 });
